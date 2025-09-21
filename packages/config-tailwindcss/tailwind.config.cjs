30ce535c540643d6405d041498d437fbca90acde
Nickolas Kenyeres <dev@kenyeres.ca>
2025-09-21 19:59:29 -0400

    Setup tailwind package

---
+++ packages/config-tailwindcss/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Leave content empty. Paths will be defined in apps/packages
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
