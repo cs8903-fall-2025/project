3cf4a436b794ae94d9bd6e0a3796e68ab30b2483
Nickolas Kenyeres <dev@kenyeres.ca>
2025-10-19 16:52:48 -0400

    Setup astro for app

---
+++ apps/app/eslint.config.mjs
@@ -0,0 +1,11 @@
+import { baseConfig } from '@repo/config-eslint/base-config'
+import astroBaseConfig from 'eslint-plugin-astro'
+
+/** @type {import('eslint').Linter.Config} */
+export default [
+  ...baseConfig,
+  ...astroBaseConfig.configs.recommended,
+  {
+    ignores: ['.astro', '.turbo', 'dist/**', 'public'],
+  },
+]
