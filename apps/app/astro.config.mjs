a70a50e881a80b0e3711a1e8e146931905892dcc
Nickolas Kenyeres <dev@kenyeres.ca>
2025-10-19 20:05:13 -0400

    Add button to page

--- apps/app/astro.config.mjs
+++ apps/app/astro.config.mjs
@@ -1,4 +1,6 @@
 // @ts-check
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'astro/config'
 
 import AstroPWA from '@vite-pwa/astro'
@@ -7,11 +9,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 import react from '@astrojs/react'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   integrations: [AstroPWA(), react()],
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   },
 })
