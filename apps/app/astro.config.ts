053c9fc11044365a9bbd578e27db13f798b5f3fa
Nickolas Kenyeres <dev@kenyeres.ca>
2025-10-19 22:10:25 -0400

    Working PWA

---
+++ apps/app/astro.config.ts
@@ -0,0 +1,52 @@
+// @ts-check
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'astro/config'
+
+import AstroPWA from '@vite-pwa/astro'
+import cloudflare from '@astrojs/cloudflare'
+import tailwindcss from '@tailwindcss/vite'
+
+import react from '@astrojs/react'
+
+import manifest from './config/manifest'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest,
+      pwaAssets: {
+        config: true,
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/$/],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+    react(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  },
+})
