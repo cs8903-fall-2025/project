8fb9ce48889cd1feee1caab6c1d910dc18c18759
Nickolas Kenyeres <dev@kenyeres.ca>
2025-11-20 07:35:37 -0500

    Setup eslint config

--- apps/app/src/pwa.ts
+++ apps/app/src/pwa.ts
@@ -3,11 +3,11 @@ import { registerSW } from 'virtual:pwa-register'
 registerSW({
   immediate: true,
   onRegisteredSW(swScriptUrl: string) {
-    // eslint-disable-next-line no-console
+     
     console.log('SW registered: ', swScriptUrl)
   },
   onOfflineReady() {
-    // eslint-disable-next-line no-console
+     
     console.log('PWA application ready to work offline')
   },
 })
