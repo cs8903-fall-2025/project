5646cecd8dc8a383de0cc4f7698c39854bd54490
Nickolas Kenyeres <dev@kenyeres.ca>
2025-11-20 09:04:04 -0500

    Setup router

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -3,15 +3,25 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react(),
     tailwindcss(),
-    VitePWA({ devOptions: { enabled: true }, registerType: 'autoUpdate' }),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      registerType: 'autoUpdate',
+    }),
     cloudflare(),
   ],
   resolve: {
