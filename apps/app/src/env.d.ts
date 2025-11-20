d70f8d630944b8221977153b3c9dc5c1f300a5da
Nickolas Kenyeres <dev@kenyeres.ca>
2025-11-20 08:37:32 -0500

    Replace app with new PWA

--- apps/app/src/env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="astro/client" />
-/// <reference types="vite-plugin-pwa/astro" />
-/// <reference types="vite-plugin-pwa/info" />
-/// <reference types="vite-plugin-pwa/pwa-assets" />
-/// <reference types="vite-plugin-pwa/pwa-register" />
