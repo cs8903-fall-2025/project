6f610bc2ec7b013a46a07c99e8959d34816c1711
Nickolas Kenyeres <dev@kenyeres.ca>
2025-12-01 23:10:43 -0500

    Update design

--- apps/app/src/routes/__root.tsx
+++ apps/app/src/routes/__root.tsx
@@ -7,28 +7,35 @@ export const Route = createRootRoute({
 
 function RootComponent() {
   return (
-    <div className="p-6">
-      <div className="p-2 flex gap-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Assessments
-        </Link>{' '}
-        <Link
-          to="/assessments/new"
-          activeProps={{
-            className: 'font-bold',
-          }}
-        >
-          New Assessment
-        </Link>
-      </div>
-      <Outlet />
+    <>
+      <header className="bg-accent py-3 px-6 flex items-center space-between">
+        <h1 className="text-primary text-lg font-semibold flex-1">
+          AutoGrader
+        </h1>
+        <nav className="p-2 flex gap-2">
+          <Link
+            to="/"
+            activeProps={{
+              className: 'font-bold',
+            }}
+            activeOptions={{ exact: true }}
+          >
+            Assessments
+          </Link>{' '}
+          <Link
+            to="/assessments/new"
+            activeProps={{
+              className: 'font-bold',
+            }}
+          >
+            New Assessment
+          </Link>
+        </nav>
+      </header>
+      <main className="py-8 px-6">
+        <Outlet />
+      </main>
       <TanStackRouterDevtools position="bottom-right" />
-    </div>
+    </>
   )
 }
