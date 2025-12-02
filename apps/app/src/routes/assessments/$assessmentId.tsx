8e04b8096c364ff63f6380360ef55c71201dae71
Nickolas Kenyeres <dev@kenyeres.ca>
2025-12-02 09:04:49 -0500

    Fix breadcrumb

--- apps/app/src/routes/assessments/$assessmentId.tsx
+++ apps/app/src/routes/assessments/$assessmentId.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, useNavigate, Link } from '@tanstack/react-router'
 import { useFetchAssessment } from '@/hooks/useFetchAssessment'
 
 import { Button } from '@/components/ui/button'
+import { House, Slash } from 'lucide-react'
 
 export const Route = createFileRoute('/assessments/$assessmentId')({
   component: RouteComponent,
@@ -18,12 +19,13 @@ function RouteComponent() {
   return (
     <>
       <nav aria-label="Breadcrumb" className="flex mb-3">
-        <ol className="flex items-center space-x-4">
+        <ol className="flex items-center space-x-1">
           <li>
             <div className="flex items-center">
+              <House className="text-muted-foreground size-3" />
               <Link
                 to="/"
-                className="text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                className="ml-1 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
               >
                 Assessments
               </Link>
@@ -31,17 +33,10 @@ function RouteComponent() {
           </li>
           <li>
             <div className="flex items-center">
-              <svg
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-                className="size-5 shrink-0 text-gray-300 dark:text-gray-600"
-              >
-                <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
-              </svg>
+              <Slash className="text-muted-foreground size-3" />
               <Link
                 aria-current="page"
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                className="ml-1 text-sm font-medium text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
                 to={`/assessments/${assessmentId}`}
               >
                 Submissions
