66b981d7a2d4e35d58c950462d236a6b09b4357c
Nickolas Kenyeres <dev@kenyeres.ca>
2025-12-07 12:36:57 -0500

    Design header

--- apps/app/src/routes/__root.tsx
+++ apps/app/src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Plus } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,28 +10,46 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <header className="bg-accent py-3 px-6 flex items-center space-between">
-        <h1 className="text-primary text-lg font-semibold flex-1">
-          AutoGrader
+      <header className="bg-primary py-3 px-6 flex items-center space-between">
+        <img
+          src="/logo-round.png"
+          alt="Logo"
+          className="h-14 w-14 rounded-full mr-2 border-2 border-white"
+        />
+        <h1 className="text-primary-foreground text-lg font-semibold flex-1">
+          Owligent
         </h1>
-        <nav className="p-2 flex gap-2">
-          <Link
-            to="/"
-            activeProps={{
-              className: 'font-bold',
-            }}
-            activeOptions={{ exact: true }}
+        <nav className="text-primary-foreground p-2 flex gap-4 items-center">
+          <Button
+            asChild
+            variant="link"
+            className="text-primary-foreground hover:text-primary-foreground"
           >
-            Assessments
-          </Link>{' '}
-          <Link
-            to="/assessments/new"
-            activeProps={{
-              className: 'font-bold',
-            }}
+            <Link
+              to="/"
+              activeProps={{
+                className: 'font-bold',
+              }}
+              activeOptions={{ exact: true }}
+            >
+              Assessments
+            </Link>
+          </Button>
+          <Button
+            asChild
+            variant="secondary"
+            className="border-white border bg-transparent hover:bg-blue-800 text-primary-foreground hover:text-primary-foreground"
           >
-            New Assessment
-          </Link>
+            <Link
+              to="/assessments/new"
+              activeProps={{
+                className: 'font-bold',
+              }}
+            >
+              <Plus aria-hidden="true" />
+              New Assessment
+            </Link>
+          </Button>
         </nav>
       </header>
       <main className="py-8 px-6">
